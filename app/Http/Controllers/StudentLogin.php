2d22d685275064072221f89c1d5d1ad34561ad52
karanawate <karanawate8@gmail.com>
2021-11-29 18:34:06 +0530

    Login system with otp send testing

--- app/Http/Controllers/StudentLogin.php
+++ app/Http/Controllers/StudentLogin.php
@@ -59,10 +59,102 @@ public function forgetPassword()
      return view('web_forgetPassword');    
 }
 
-public function otpSend()
+public function otpSend(Request $request)
 {
-    echo "Hello";
+
+    $UserMobile = $request->UserMobile;
+
+    $query      = DB::table('tbl_login')
+                ->where('UserMobile',$UserMobile)
+                ->get();
+
+    if(!$query->isEmpty($query))
+    {
+      
+        $UserMobile = $request->UserMobile;
+        $message    = trim("is Your OTP for VEDIC TREE KIDS LEARNING APP login For further details please visit our website www.vedictreeschool.online");
+        $otpNumber  = rand(0000,9999); 
+        $res        = $this->sendwebsms($message,$otpNumber,$UserMobile);
+    }else{
+        echo "MOBILE NO IS NOT FOUND";
+    }
 }
+
+
+
+    function sendwebsms($message,$otpNumber,$UserMobile)
+    {
+
+        $curl = curl_init();
+    
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => "http://2factor.in/API/V1/64423750-ccff-11eb-8089-0200cd936042/SMS/".$UserMobile."/".$otpNumber."/EVEDIC",
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 30,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => "GET",
+          CURLOPT_POSTFIELDS => "",
+          CURLOPT_HTTPHEADER => array(
+            "content-type: application/x-www-form-urlencoded"
+          ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+        if ($err) {
+          return $msg = "cURL Error #:" . $err;
+        } else {
+
+            $response = json_decode($response);
+            if($response->Status=="Success"){
+                return "Success";
+            }else{
+                return "fail";
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    
+
 
 
 
